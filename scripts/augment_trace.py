fb9a1676d7312cfd4fb1e454e62d5fed8dd9cc81
Chameleon Cloud User <cc@as-sim.novalocal>
2023-07-07 22:52:12 +0000

    ransomised demands

--- scripts/augment_trace.py
+++ scripts/augment_trace.py
@@ -3,6 +3,7 @@ import json
 import random
 
 models = ["VGG", "ResNet50"]
+demands =["1", "2", "4", "8", "16"]
 
 #in_file= open(sys.argv[1], "r")
 #out_file = open(sys.argv[2], "w")
@@ -14,6 +15,7 @@ for trace in traceJson:
     if "model" not in trace.keys():
         trace["model"] = random.choice(models)
     trace["total_iterations"] = str(int(trace["total_iterations"]) * 100)
+    trace["max_parallelism"] = random.choice(demands)
 
 fd.close()
 
